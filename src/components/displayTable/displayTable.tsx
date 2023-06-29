32e9fa2c7580487b0ad31bdff534c7e99e114ffe
Gareth Trufitt <gtrufitt@users.noreply.github.com>
2023-06-29 22:08:15 +0100

    Update displayTable.tsx

--- src/components/displayTable/displayTable.tsx
+++ src/components/displayTable/displayTable.tsx
@@ -8,13 +8,13 @@ export default function Table(props: { data: DocsDataType; canEdit: boolean }) {
     <table className="my-6">
       {props.data.map((row) => (
         <tr className="text-black px-2 border-b-2" key={row.rowNum} data-row-num={row.rowNum}>
-          <td className="pr-4">
+          <td className="pr-2">
             {moment(row.date, "D/MM/yyyy").format("ddd Do MMM")}
           </td>
-          <td className="pr-4">
+          <td className="pr-2">
             {row.shiftStart} - {row.shiftEnd}
           </td>
-          <td className="pr-4">
+          <td className="pr-1">
             <span className="text-sm text-gray-500">
               {row.hours} hours at {row.enhancedRate || "£15:00"}
               {row.enhancedRate && "✨"}

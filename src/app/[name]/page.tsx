146394e61d8e71abdce909a96bafd5e9703e711c
Gareth Trufitt <gtrufitt@gmail.com>
2023-06-29 09:03:29 +0100

    Split shifts to upcoming and past

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import moment from "moment";
 
 export default async function Name({ params }: { params: { name: string } }) {
   const loggedInCookie: { name: string; username: string } = JSON.parse(
@@ -17,9 +18,23 @@ export default async function Name({ params }: { params: { name: string } }) {
 
   const data = await DocsData();
 
-  const availableShifts = data.filter(
+  const carerShifts = data.filter(
     (row) => row.name?.toLowerCase() === params.name.toLowerCase()
   );
+
+  const shiftsBeforeToday = carerShifts.filter((row) =>
+    moment(row.date, "D/MM/yyyy").isBefore(moment())
+  );
+
+  const shiftsAfterToday = carerShifts.filter((row) =>
+    moment(row.date, "D/MM/yyyy").isAfter(moment())
+  );
+
+  const totalHours = shiftsBeforeToday.reduce(
+    (prev, curr) => prev + curr.hours,
+    0
+  );
+
   return (
     <main className="min-h-screen p-6">
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
@@ -33,7 +48,15 @@ export default async function Name({ params }: { params: { name: string } }) {
       >
         See available shifts
       </Link>
-      <Table data={availableShifts} canEdit={false} />
+      <h2 className="mb-4 mt-4 text-2xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Upcoming Shifts
+      </h2>
+      <Table data={shiftsAfterToday} canEdit={false} />
+      <h2 className="mb-4 mt-4 text-2xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Past Shifts
+      </h2>
+      <Table data={shiftsBeforeToday} canEdit={false} />
+      <p className={"mt-4 "}>Total Hours: {totalHours}</p>
     </main>
   );
 }

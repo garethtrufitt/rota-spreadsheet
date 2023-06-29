cbfe4a11c77534810d99202fc87a523f91d5db08
Gareth Trufitt <gtrufitt@gmail.com>
2023-06-29 14:00:16 +0100

    Update display table display

--- src/components/displayTable/displayTable.tsx
+++ src/components/displayTable/displayTable.tsx
@@ -1,45 +1,32 @@
+import moment from "moment";
 import { DocsDataType } from "../../dataUpdates/getSheetData";
 import EditButton from "../editButton";
 
 export default function Table(props: { data: DocsDataType; canEdit: boolean }) {
   return (
-    <table className="border-collapse w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <tr className="border-y-2">
-          <th scope="col" className="px-1 py-1">
-            Date
-          </th>
-          <th scope="col" className="px-1 py-1">
-            Shift Start
-          </th>
-          <th scope="col" className="px-1 py-1">
-            Shift End
-          </th>
-          <th scope="col" className="px-1 py-1">
-            Hours
-          </th>
-          <th scope="col" className="px-1 py-1">
-            Enhanced Rate
-          </th>
-          {props.canEdit && (
-            <th scope="col" className="px-1 py-1">
-              Select shift
-            </th>
-          )}
-        </tr>
-      </thead>
-      <tbody>
-        {props.data.map((row) => (
-          <tr className="border-y-2" key={row.rowNum} data-row-num={row.rowNum}>
-            <td className="px-2 py-2">{row.date}</td>
-            <td className="px-2 py-2">{row.shiftStart}</td>
-            <td className="px-2 py-2">{row.shiftEnd}</td>
-            <td className="px-2 py-2">{row.hours}</td>
-            <td className="px-2 py-2">{row.enhancedRate}</td>
-            {props.canEdit && <EditButton rowNum={row.rowNum} />}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <dl className="max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
+      {props.data.map((row) => (
+        <div
+          className="flex flex-col pb-3"
+          key={row.rowNum}
+          data-row-num={row.rowNum}
+        >
+          <dt className="mb-1 font-semibold md:text-lg dark:text-gray-400">
+            {moment(row.date, "D/MM/yyyy").format("ddd Do MMM")} (
+            {row.shiftStart} - {row.shiftEnd}){" "}
+            <span className="text-sm font-thin text-gray-500">
+              {row.hours} hours at {row.enhancedRate || "£15:00"}{" "}
+              {row.enhancedRate && "✨"}
+            </span>
+            {props.canEdit && (
+              <label className="float-right">
+                <EditButton rowNum={row.rowNum} />
+              </label>
+            )}
+          </dt>
+          <dd className="text-m text-gray-500"></dd>
+        </div>
+      ))}
+    </dl>
   );
 }
